e8f0f26f8d851db9d410891afbc88a69286804fe
xrgayathri2 <xrgayathri2@LDO-PKT-PC0038.ldo.net>
2019-02-22 10:38:49 +0000

    find duplicate updated

--- Selenium1/src/main/java/week2/FindDuplicate.java
+++ Selenium1/src/main/java/week2/FindDuplicate.java
@@ -3,46 +3,43 @@ package week2;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 public class FindDuplicate {
 
 	public static void main(String[] args) {
-		
-		
+
+
 
 		List<Integer> ls=new ArrayList<>();
-		
+
 		ls.add(1);
 		ls.add(1);
 		ls.add(3);
 		ls.add(3);
 		ls.add(2);
 		ls.add(2);
+		ls.add(4);
 		Set<Integer> set=new HashSet<Integer>();
-		set.addAll(ls);
 		System.out.println("With duplicates-->"+ls);
-		//System.out.println("without duplicate-->"+set);
-		List<Integer> ls1=new ArrayList<>();
-		ls1.addAll(set);
 
-		for (int i = 0; i < ls1.size(); i++) {
-			int c=0;
-			for (int j = i+1; j < ls.size(); j++) {
-				if(ls1.get(i)==ls.get(j)) {
-					c++;
-				}
+		boolean flag=true;
+		Set <Integer> dset=new LinkedHashSet<>();
 
+		for (int i = 0; i < ls.size(); i++) {
+			flag=set.add(ls.get(i));
+			if(flag==false) {
+				dset.add(ls.get(i));
 			}
-			if(c>=1) {
-				System.out.print(ls1.get(i)+" ");
-			}
-			//			else {
-			//				System.out.println("no");
-			//			}
 		}
+		System.out.println("without duplicates-->"+set);
+		System.out.println("duplicates-->" +dset);
+
 
 	}
 
 }
+
+
